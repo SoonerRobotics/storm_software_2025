0b1905d80ad934c9b18a11d3774c6425dd719ba4
Vinh Nguyen <vinnynguyen2006@gmail.com>
2024-11-11 19:08:56 -0600

    finally make this thing work

--- src/operator/operator_main.py
+++ src/operator/operator_main.py
@@ -1,17 +1,20 @@
 from aiohttp import web
 routes = web.RouteTableDef()
 
-from rtcbot import RTCConnection, getRTCBotJS, CVCamera
+from rtcbot import RTCConnection, getRTCBotJS
 
-camera = CVCamera()
 # For this example, we use just one global connection
 conn = RTCConnection()
-conn.video.putSubscription(camera)
+
+@conn.subscribe
+def onMessage(msg):  # Called when each message is sent
+    print("Got message:", msg)
 
 # Serve the RTCBot javascript library at /rtcbot.js
 @routes.get("/rtcbot.js")
 async def rtcbotjs(request):
     return web.Response(content_type="application/javascript", text=getRTCBotJS())
+
 
 # This sets up the connection
 @routes.post("/connect")
@@ -20,14 +23,55 @@ async def connect(request):
     serverResponse = await conn.getLocalDescription(clientOffer)
     return web.json_response(serverResponse)
 
+
 @routes.get("/")
 async def index(request):
-    with open("index.html", "r") as f:
-        return web.Response(content_type="text/html", text=f.read())
+    return web.Response(
+        content_type="text/html",
+        text="""
+    <html>
+        <head>
+            <title>RTCBot: Data Channel</title>
+            <script src="/rtcbot.js"></script>
+        </head>
+        <body style="text-align: center;padding-top: 30px;">
+            <h1>Click the Button</h1>
+            <button type="button" id="mybutton">Click me!</button>
+            <p>
+            Open the browser's developer tools to see console messages (CTRL+SHIFT+C)
+            </p>
+            <script>
+                var conn = new rtcbot.RTCConnection();
+
+                async function connect() {
+                    let offer = await conn.getLocalDescription();
+
+                    // POST the information to /connect
+                    let response = await fetch("/connect", {
+                        method: "POST",
+                        cache: "no-cache",
+                        body: JSON.stringify(offer)
+                    });
+
+                    await conn.setRemoteDescription(await response.json());
+
+                    console.log("Ready!");
+                }
+                connect();
+
+
+                var mybutton = document.querySelector("#mybutton");
+                mybutton.onclick = function() {
+                    conn.put_nowait("Button Clicked!");
+                };
+            </script>
+        </body>
+    </html>
+    """)
 
 async def cleanup(app=None):
     await conn.close()
-    camera.close()
+
 
 app = web.Application()
 app.add_routes(routes)
