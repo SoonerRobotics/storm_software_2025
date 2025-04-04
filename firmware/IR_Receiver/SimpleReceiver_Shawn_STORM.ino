0906c7edea3409b4c3ad82cc4b3811b34c00651c
dishishshawn <shawnagarwal0@gmail.com>
2025-04-03 19:57:09 -0500

    Update SimpleReceiver_Shawn_STORM.ino

--- firmware/IR_Receiver/SimpleReceiver_Shawn_STORM.ino
+++ firmware/IR_Receiver/SimpleReceiver_Shawn_STORM.ino
@@ -6,6 +6,7 @@
 
 void setup() {
     const int IR_RECEIVE_PIN1 = 15;
+    Serial.begin(115200);  
     Wire.setSDA(4);
     Wire.setSCL(5);
     Wire.begin(2);
@@ -14,11 +15,11 @@ void setup() {
     IrReceiver.begin(IR_RECEIVE_PIN1, ENABLE_LED_FEEDBACK);
 }
 
-int byteSending = 1;
-int toTransfer = 5;
-int Shift = toTransfer;
-int mask = 0xFF;
-unsigned char toSend = 0;
+// int byteSending = 1;
+uint toTransfer = 0;
+// int Shift = toTransfer;
+// int mask = 0xFF;
+// unsigned char toSend = 0;
 
 
 void loop() {
@@ -31,36 +32,40 @@ void loop() {
 
 void requestInt()
 {
-  if (byteSending == 1) //send packet 1
-  {
-    toSend = Shift & mask;
-    Shift = Shift >> 8;
-    Wire.write(toSend);
-    byteSending = 2;
-  }
-  else if (byteSending == 2) //send packet 2
-  {
-    toSend = Shift & mask;
-    Shift = Shift >> 8;
-    Wire.write(toSend);
-    byteSending = 3;
-  }
-  else if (byteSending == 3) //send packet 3
-  {
-    toSend = Shift & mask;
-    Shift = Shift >> 8;
-    Wire.write(toSend);
-    byteSending = 4;
-  }
-  else if (byteSending == 4) //send packet 4
-  {
-    toSend = Shift & mask;
-    Shift = Shift >> 8;
-    Wire.write(toSend);
-    byteSending = 1;
-    //initialization for next turn
-    Shift = toTransfer;
-    mask = 0xFF;
-    toSend = 0;
-  }
+
+  Wire.write(toTransfer);
+  toTransfer = 0;
+
+  // if (byteSending == 1) //send packet 1
+  // {
+  //   toSend = Shift & mask;
+  //   Shift = Shift >> 8;
+  //   Wire.write(toSend);
+  //   byteSending = 2;
+  // }
+  // else if (byteSending == 2) //send packet 2
+  // {
+  //   toSend = Shift & mask;
+  //   Shift = Shift >> 8;
+  //   Wire.write(toSend);
+  //   byteSending = 3;
+  // }
+  // else if (byteSending == 3) //send packet 3
+  // {
+  //   toSend = Shift & mask;
+  //   Shift = Shift >> 8;
+  //   Wire.write(toSend);
+  //   byteSending = 4;
+  // }
+  // else if (byteSending == 4) //send packet 4
+  // {
+  //   toSend = Shift & mask;
+  //   Shift = Shift >> 8;
+  //   Wire.write(toSend);
+  //   byteSending = 1;
+  //   //initialization for next turn
+  //   Shift = toTransfer;
+  //   mask = 0xFF;
+  //   toSend = 0;
+  // }
 }
