1ae44c4da818dc6576e5b001f28f32bdd929140d
braden624 <bradenjames12@icloud.com>
2025-04-05 05:28:10 -0500

    Please work (Serial to UDP)

--- operator/src/modules/messages.py
+++ operator/src/modules/messages.py
@@ -42,19 +42,11 @@ class RobotMessages(QThread):
         while self.running:
             try:
                 data, addr = self.client_socket.recvfrom(1024)
-                message_type = data[0]
-                if message_type == 1:
-                    if len(data) >= 3:
-                        sonar_id = chr(data[1])
-                        if sonar_id == 'R':
-                            self.right_sonar = data[2]
-                        elif sonar_id == 'L':
-                            self.left_sonar = data[2]
-                        elif sonar_id == 'B':
-                            self.back_sonar = data[2]
-                elif message_type == 2:
-                    if len(data) >= 3:
-                        infrared = struct.unpack("!H", data[1:3])[0]
+                if len(data) == 4:
+                    ir_code = struct.unpack('<I', data)[0]
+                    self.robot_update.emit(f'Infrared: {ir_code}');
+                else:
+                    print(f"Unexpected data size: {len(data)} bytes: {data}")
                 self.log_update.emit(helpers.log(f'Received data from {addr}: {data}', self.name))
                 self.robot_state(1)
             except Exception as e:
